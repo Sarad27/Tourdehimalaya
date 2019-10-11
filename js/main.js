5d2db5ae0b8e9ab86f7ac17bbc57ecc243f092e8
Sarad27 <47153910+Sarad27@users.noreply.github.com>
2019-10-11 21:34:19 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+// For scroll back to top
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+});
+
+$('.carousel').carousel({
+  interval: 3000
+});
